d04d6c78d0ff1fd82f78cc04496eaddf23e0dfae
Kirill Sonk <ikirsonk@gmail.com>
2018-10-06 15:21:41 +0300

    Added pug views

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,11 +1,11 @@
 'use strict';
 
 import { Form } from "./components/form.js";
+import about from '../views/about.js';
 
 const httpReq = window.httpModule;
 
 const root = document.getElementById("root");
-
 function createLinkMenu() {
     const link = document.createElement('a');
     link.textContent = "Back to main menu";
@@ -69,6 +69,18 @@ function createSignIn() {
     header.id = "header";
     root.appendChild(header);
 
+    httpReq.doGet({
+        callback(res) {
+            if (res.status > 300) {
+                alert("You already logged in");
+                root.innerHTML = '';
+                createMenu();
+                return;
+            }
+        },
+        url: '/signin'
+    });
+
     const logo = document.createElement('span');
     logo.id = 'logo';
     const headerTitle = document.createElement('h1');
@@ -77,7 +89,6 @@ function createSignIn() {
 
     header.appendChild(logo);
     header.appendChild(headerTitle);
-
 
     const body = document.createElement('div');
     body.id = 'body';
@@ -122,6 +133,11 @@ function createSignIn() {
             alert("Enter email!")
             return
         }
+
+        let formData = new FormData()
+        formData.append("password", password)
+        formData.append("email", email)
+
         httpReq.doPost({
             url: '/signin',
             callback(res) {
@@ -130,16 +146,18 @@ function createSignIn() {
                     return;
                 }
                 if(res.status == 400){
-                    alert("Already loged in")
+                    alert("Wrong email or password")
                 }
-                createProfile();
+                if(res.status == 200){
+                    alert("You are log in!")
+                    createProfile();
+                }
+                //createProfile();
             },
-            data: {
-                email,
-                password
-            }
+            data: formData
         })
     });
+
 }
 
 function createSignUp() {
@@ -159,6 +177,17 @@ function createSignUp() {
     header.appendChild(logo);
     header.appendChild(headerTitle);
 
+    httpReq.doGet({
+        callback(res) {
+            if (res.status > 300) {
+                alert("You already register");
+                root.innerHTML = '';
+                createMenu();
+                return;
+            }
+        },
+        url: '/signup'
+    });
 
     const body = document.createElement('div');
     body.id = 'body';
@@ -168,6 +197,18 @@ function createSignUp() {
     formblock.id = 'formblock';
     body.appendChild(formblock)
     const inputs = [
+      {
+          name: 'name',
+          placeholder: 'Name'
+      },
+      {
+          name: 'last_name',
+          placeholder: 'Last name'
+      },
+      {
+          name: 'nick',
+          placeholder: 'Nick'
+      },
         {
             name: 'email',
             type: 'email',
@@ -202,7 +243,9 @@ function createSignUp() {
     form.addEventListener('submit', function (event) {
 
         event.preventDefault();
-
+        const name = form.elements['name'].value;
+        const last_name = form.elements['last_name'].value;
+        const nick = form.elements['nick'].value;
         const email = form.elements['email'].value;
         const password = form.elements['password'].value;
         const password_repeat = form.elements['password_repeat'].value;
@@ -216,25 +259,37 @@ function createSignUp() {
             return
         }
 
+        let formData = new FormData()
+        formData.append("name", name)
+        formData.append("last_name", last_name)
+        formData.append("nick", nick)
+
+        formData.append("password", password)
+        formData.append("email", email)
+
+
         httpReq.doPost({
             callback(res) {
+              console.log(res.status)
                 if (res.status == 208){
                     alert("Email already exist");
                     return;
                 }
-                if (res.status > 300) {
-                    alert("Something was wrong");
+                if (res.status == 400) {
+                    alert("Something is wrong");
+                    return;
+                }
+                if (res.status == 409) {
+                    alert("StatusConflict");
                     return;
                 }
                 createProfile();
             },
             url: '/signup',
-            data: {
-                "email":email,
-                "password":password,
-            }
+            data: formData
         });
     });
+
 }
 
 function createLeaders() {
@@ -285,61 +340,67 @@ function createLeaders() {
       em.textContent = 'Nothing to display';
       body.appendChild(em);*/
 
-    xhr.open('POST', '/liderboards', true);
-    xhr.setRequestHeader('Content-Type', 'text/plain; charset=utf-8');
-    xhr.send('Request');
+    // xhr.open('POST', '/liderboards', true);
+    // xhr.setRequestHeader('Content-Type', 'text/plain; charset=utf-8');
+    // xhr.send('Request');
 
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState !== 4) return;
-        if (xhr.status !== 200) {
-            alert(xhr.status + ': ' + xhr.statusText);
-        } else {
-            const top = JSON.parse(xhr.responseText);
-            /*const d1 = document.createElement('div');
-            const d = document.createElement('h1');
 
-            d.innerText = text;
-            d1.appendChild(d);
-
-            root.appendChild(d1);*/
+    httpReq.doGet({
+      callback(res) {
+          if (res.status > 300) {
+              alert('Something wrong');
+              root.innerHTML = '';
+              createMenu();
+              return;
+          }
+          res.json().then(function(top) {
 
             const tbody = document.createElement('tbody');
 
             let username;
             let score;
+            let age;
 
             Object.entries(top).forEach(function ([id, info]) {
-                username = info.nickname;
-                score = info.score;
+                username = info.nick;
+                score = info.Score;
+                age = info.Age;
 
 
                 const tr = document.createElement('tr');
                 const tdUsername = document.createElement('td');
                 const tdScore = document.createElement('td');
-                // const tdAge = document.createElement('td');
+                 const tdAge = document.createElement('td');
 
                 tdUsername.textContent = username;
                 tdScore.textContent = score;
-                //   tdAge.textContent = age;
+                   tdAge.textContent = age;
 
                 tr.appendChild(tdUsername);
                 tr.appendChild(tdScore);
-                // tr.appendChild(tdAge);
+                 tr.appendChild(tdAge);
 
                 tbody.appendChild(tr);
 
                 table.appendChild(tbody);
-
+              });
             });
-        }
-    };
+          },
+  url: '/leaders'
+
+});
+
+
+
+
+
 
     body.appendChild(table);
     root.appendChild(body);
 
 }
 
-function createProfile() {
+function createProfile(me) {
     root.innerHTML = '';
     const logo = document.createElement('div');
     logo.classList.add("p_name");
@@ -354,47 +415,151 @@ function createProfile() {
     body.id = 'body';
     root.appendChild(body);
 
-    const pLink = document.createElement('p');
-    body.appendChild(pLink);
 
-    const link = createLinkMenu()
-    pLink.appendChild(link);
+    if (me){
+      const formblock = document.createElement('div');
+      const img = document.createElement('img');
+      img.src="static/media/" + me.Uid;
+      body.appendChild(img);
+
+      //Вывод имени, фамилии!
+
+      formblock.id = 'formblock';
+      body.appendChild(formblock);
+
+      const inputs = [
+          {
+              name:'my_file',
+              type:'file'
+          },
+          {
+              name: 'nick',
+              placeholder: me.nick
+          },
+          {
+              name: 'password',
+              type: 'password',
+              placeholder: 'Password'
+          },
+          {
+              name: 'password_repeat',
+              type: 'password',
+              placeholder: 'Repeat Password'
+          },
+          {
+              name: 'submit',
+              type: 'submit',
+              value: 'Submit'
+          }
+      ];
+      const formObj = new Form();
+      const form = formObj.render({ inputs: inputs, formId: 'profileForm' })
+      formblock.appendChild(form);
+
+      const pLink = document.createElement('p');
+      form.appendChild(pLink);
+
+      const link = createLinkMenu();
+      pLink.appendChild(link);
+
+
+      form.addEventListener('submit', function (event) {
+
+      event.preventDefault();
+
+      let formData = new FormData(document.forms.profileForm);
+           //file is actually new FileReader.readAsData(myId.files[0]);
+      //  formData.append("my_file", avatar);
+
+      const password = form.elements['password'].value;
+      const password_repeat = form.elements['password_repeat'].value;
+
+      if (password !== password_repeat) {
+          alert('Passwords is not equals');
+          return;
+        }
+
+        formData.append("password", password)
+
+
+          httpReq.doPost({ // Отправка аватарки
+              callback(res) {
+                  if (res.status > 300) {
+                      alert("Something was wrong");
+                      return;
+                  }
+                  createProfile();
+              },
+              url: '/profile',
+              data: formData
+          });
+
+      });
+    } else {
+      httpReq.doGet({
+        callback(res) {
+          console.log("Create prof")
+          console.log(res.status);
+            if (res.status > 300) {
+                alert('Unauthorized');
+                root.innerHTML = '';
+					      createMenu();
+                return;
+            }
+            //let respon = res.json();
+            //const user = JSON.parse(res.responseText);
+
+            res.json().then(function(user) {
+              root.innerHTML = '';
+              createProfile(user);
+      });
+
+        },
+        url: '/profile'
+      })
+    }
+
+
+
+
+
 }
 
 function createAbout() {
-    root.innerHTML = '';
-    const header = document.createElement('div');
-    header.id = "header";
-    header.dataset.sectionName = 'header';
-    root.appendChild(header);
+    const html = about()
+    root.innerHTML = html;
+    // const header = document.createElement('div');
+    // header.id = "header";
+    // header.dataset.sectionName = 'header';
+    // root.appendChild(header);
 
-    const logo = document.createElement('span');
-    logo.id = 'logo';
-    const headerTitle = document.createElement('h1');
-    headerTitle.textContent = 'About';
-    headerTitle.id = "headerTitle";
+    // const logo = document.createElement('span');
+    // logo.id = 'logo';
+    // const headerTitle = document.createElement('h1');
+    // headerTitle.textContent = 'About';
+    // headerTitle.id = "headerTitle";
 
-    header.appendChild(logo);
-    header.appendChild(headerTitle);
+    // header.appendChild(logo);
+    // header.appendChild(headerTitle);
 
-    const body = document.createElement('div');
-    body.id = 'body';
+    // const body = document.createElement('div');
+    // body.id = 'body';
 
-    const aboutTextblock = document.createElement('div');
-    body.appendChild(aboutTextblock);
+    // const aboutTextblock = document.createElement('div');
+    // body.appendChild(aboutTextblock);
 
-    const aboutText = document.createElement('p');
-    aboutText.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    aboutTextblock.appendChild(aboutText);
+    // const aboutText = document.createElement('p');
+    // aboutText.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    // aboutTextblock.appendChild(aboutText);
 
-    const pLink = document.createElement('p');
-    body.appendChild(pLink);
+    // const pLink = document.createElement('p');
+    // body.appendChild(pLink);
 
-    const link = createLinkMenu()
-    pLink.appendChild(link);
+    // const link = createLinkMenu()
+    // pLink.appendChild(link);
 
 
-    root.appendChild(body);
+    // root.appendChild(body);
 }
 
 const buttons = {
@@ -415,7 +580,6 @@ root.addEventListener("click", function (event) {
     const href = target.dataset.href;
 
     root.innerHTML = '';
-    console.log(href);
     buttons[href]();
 
 });
