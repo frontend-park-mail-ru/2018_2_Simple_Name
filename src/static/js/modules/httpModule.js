f7fa19edaf4b474015058719b0637fe34e41edee
Kirill Sonk <ikirsonk@gmail.com>
2018-10-19 14:50:23 +0300

    deleted unused code

--- src/static/js/modules/httpModule.js
+++ src/static/js/modules/httpModule.js
@@ -6,9 +6,6 @@
                 method: method,
                 mode: 'cors',
                 credentials: 'include',
-                //headers: {
-                //    "Content-Type": "multipart/form-data; boundary=something",//"application/json; charset=utf-8",
-                //  },
                 body: data,
             })
                 .then(function (res) {
@@ -28,27 +25,3 @@
     }
     window.httpModule = new httpReq();
 })();
-
-    // class httpRequset {
-    //     _dofetch({ url = '/', method = 'GET', data, callback = function () { } } = {}) {
-    //         fetch(url, {
-    //             method: method,
-    //             mode: 'cors',
-    //             credentials: 'include',
-    //             body: data,
-    //         })
-    //             .then(function (res) {
-    //                 callback(res);
-    //             })
-    //             .catch(function (err) {
-    //                 console.log(err);
-    //             });
-    //     }
-    //     doGet(params = {}) {
-    //         this._dofetch({ ...params, method: 'GET' });
-    //     }
-
-    //     doPost(params = {}) {
-    //         this._dofetch({ ...params, method: 'POST' });
-    //     }
-    // }
