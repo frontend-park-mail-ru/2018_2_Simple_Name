65388c4adfe296b148fc0f04bdbb69cc7dea4d7a
Grisha23 <gzharkikh@mail.ru>
2018-12-11 20:20:42 +0300

    change to wss

--- src/js/modules/webSocketService.js
+++ src/js/modules/webSocketService.js
@@ -1,6 +1,6 @@
 export default class WsService {
     constructor(url) {
-        this.ws = new WebSocket(`ws://${url}`);
+        this.ws = new WebSocket(`wss://${url}`);
         this.listCommands = [];
         this.ws.onmessage = (event) => {
             const msg = JSON.parse(event.data);
