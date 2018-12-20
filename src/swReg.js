88f79f6d82a45dbc927dfef8ac8025631533b197
Grisha23 <gzharkikh@mail.ru>
2018-12-20 17:08:44 +0300

    sw ready

--- src/swReg.js
+++ src/swReg.js
@@ -3,7 +3,6 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js')
         .then(() => navigator.serviceWorker.ready.then((reg) => {
             reg.sync.register('data');
-            console.log('123123123123123');
         }))
         .catch((err) => console.log('ServiceWorker registration failed: ', err));
 
